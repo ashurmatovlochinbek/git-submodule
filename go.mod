c2f52a9e4e4225f8a016d41c93673adba313ff63
ashurmatovlochinbek <126878017+ashurmatovlochinbek@users.noreply.github.com>
2024-08-04 17:29:56 +0500

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module submodule
+
+go 1.22
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ handlers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+var HandlerF1 = func(writer http.ResponseWriter, request *http.Request) {
+	uuidValue := uuid.New()
+	json, _ := json.Marshal(uuidValue)
+	writer.Write(json)
+}
